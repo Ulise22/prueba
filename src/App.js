1f18695b60f9ba1d1916f2ae208ba86c39637db6
Ulise22 <ulimontenegro23@gmail.com>
2022-06-28 20:00:57 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import About from './About';
+import Home from './Home';
+import {Route} from 'wouter'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route component={Home} path="/" />
+      <Route component={About} path="/about" />
     </div>
   );
 }
